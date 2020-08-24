1e803bb09a3a5c226bbf81a08fbf64144f2c0f21
Andrew Palmer <atp@sdf.org>
2020-08-23 22:20:09 -0500

    Stub dumps method

--- src/cjson.c
+++ src/cjson.c
@@ -1,9 +1,19 @@
 #include <Python.h>
 
+static PyObject *cjson_dumps(PyObject *self, PyObject *args, PyObject *kwargs) {
+    return NULL;
+}
+
+static PyMethodDef module_methods[] = {
+    {"dumps", cjson_dumps, METH_VARARGS | METH_KEYWORDS},
+    {0},
+};
+
 static PyModuleDef module = {
     .m_base = PyModuleDef_HEAD_INIT,
     .m_name = "cjson",
     .m_size = 0,
+    .m_methods = &module_methods,
 };
 
 PyMODINIT_FUNC PyInit_cjson(void) {
